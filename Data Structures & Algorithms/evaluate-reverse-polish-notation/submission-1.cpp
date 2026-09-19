c2e522361bedb7ee4ffc570265a139d8490d8885
Stephen Heindel <sheindel@users.noreply.github.com>
2026-09-19 11:26:48 -0400

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isOperation(const string& token) {
+        return evaluator.contains(token);
+    }
+
+    std::unordered_map<string, std::function<int(int, int)>> evaluator = {
+        { "+", [](int op1, int op2) { return (op1 + op2); }},
+        { "-", [](int op1, int op2) { return (op1 - op2); }},
+        { "*", [](int op1, int op2) { return (op1 * op2); }},
+        { "/", [](int op1, int op2) { return (op1 / op2); }},
+    };
+
+    int evaluate(const string& operation, int op1, int op2) {
+        auto result = evaluator[operation](op1, op2);
+        // std::cout << "Evauating " << op1 << operation << op2 << "=" << result << std::endl;
+        return result;
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        // single part options, +, -, *, /
+        stack<string> tokenStack;
+
+        for (const auto& token : tokens) {
+            // if operation, pop last two operands
+            if (isOperation(token)) {
+                const auto op2 = stoi(tokenStack.top());
+                tokenStack.pop();
+                const auto op1 = stoi(tokenStack.top());
+                tokenStack.pop();
+                const auto intResult = evaluate(token, op1, op2);
+                const auto strResult = std::to_string(intResult);
+                // std::cout << "Adding " << intResult << "," << strResult << " to stack" << std::endl;
+                tokenStack.push(strResult);
+            } else {
+                // std::cout << "Adding " << token << " to stack" << std::endl;
+                tokenStack.push(token);
+            }
+        }
+        return stoi(tokenStack.top());
+    }
+};
