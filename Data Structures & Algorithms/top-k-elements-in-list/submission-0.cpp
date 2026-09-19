187f638ad8fcc379cbeef6c1df12a5092eec63d4
Stephen Heindel <sheindel@users.noreply.github.com>
2026-09-19 10:27:30 -0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // [ ]++
+        // [ ]++
+        // [ ]++
+        // [ ]++
+
+        // idea 1: if frequency list is *mostly* sorted, we can do insert sort operation on 
+        // the element we just touched by comparing it the value above it
+        // then at the end, we have a sorted list of the most frequent and the answer lookup
+        // is O(1)
+        // problem is that insertion sorts will take something between O(1) O(n) every time we 
+        // insert a value, and as the frequency list grows, this likely approachs O(log n) operations
+        // possibly an ordered map, being a red black tree, could solve this somehow? 
+        // but its insertion is also not guaranteed to be O(1), so this still adds complexity
+
+        // naive solution
+        // key is number in list, value is occurrence
+        unordered_map<int, int> frequency;
+        for (const auto& number : nums) {
+            frequency[number]++; // TODO look up the assertion that the [] operator initializes a value...
+        }
+
+        // convert map to vector
+        std::vector<std::pair<int, int>> result(frequency.begin(), frequency.end());
+        std::sort(result.begin(), result.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+        auto kMostFrequent = std::vector<std::pair<int, int>>(result.begin(), result.begin() + k);
+        vector<int> result2;
+        std::transform(kMostFrequent.begin(), kMostFrequent.end(), std::back_inserter(result2), [](const auto& valueFrequency) {
+            return valueFrequency.first;
+        });
+        return result2;
+    }
+};
